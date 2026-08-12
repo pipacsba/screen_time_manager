5f4ac50fdd9f41df5c8c3cad243116f2dc39aacc
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-08-12 14:15:56 +0200

    Fix docstring

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ import logging
 
 
 def setup_logging():
-
+    """ Define basic logger config """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
